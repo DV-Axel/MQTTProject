fdc75cb83aaac4fee8d531119d1911874afda7a0
AxelcichelloArciel <acichello@arciel.com>
2025-06-17 16:47:30 -0300

    agrego comentado url para buscar por ip especifica

--- coordenadas.js
+++ coordenadas.js
@@ -4,6 +4,8 @@ const axios = require("axios");
 //Usamos una funcion basica para obetener las coordenadas
 // de la ubicacion de la estacion
 async function obtenerCoordenadas() {
+
+  //http://ip-api.com/json/24.48.0.1 cuando tenga la ip especificada
   const res = await axios.get('http://ip-api.com/json/');
 
   //console.log('Coordenadas obtenidas:', res.data);
